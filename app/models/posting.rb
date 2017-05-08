514464208f7c55d95bb42471dc5ec3d432bfdee9
seedtree_henry <seedtree.henry@gmail.com>
2017-05-08 10:37:22 +0000

    first commit

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,3 +1,8 @@
 class Posting < ActiveRecord::Base
+    
+    belongs_to :user
     has_many :comments
+    
+    validates :title, presence: true
+    validates :posting_content, presence: true
 end
